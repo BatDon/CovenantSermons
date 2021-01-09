c35f443245a2baa54c0c1000200a02fdb87f3b2b
BatDon <asics007@gmail.com>
2021-01-08 19:09:16 -0500

    feat: Database Reading sermonDatabase.kt reads the sermon data from Cloud Firestore database

---
+++ app/src/main/java/com/example/covenantsermons/MainActivity.kt
@@ -0,0 +1,17 @@
+package com.example.covenantsermons
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.covenantsermons.database.getPodcastsFromDatabase
+import timber.log.Timber
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        Timber.plant(Timber.DebugTree())
+
+        getPodcastsFromDatabase()
+    }
+}
